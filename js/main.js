d6658ffbf9a304ccf8ea71fb6a3b14e2e10043f4
MarGallito <123398118+MarGallito@users.noreply.github.com>
2023-03-06 00:01:48 +0400

    Homewor end

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// #1
+let name = 'Abel';
+// #2
+let lastName = 'Papava';
+// #3
+let myAge = 21;
+// #4
+console.log(name + ' ' + lastName);
+// #5
+let iBorn = 2002;
+// #6
+console.log('მე ვარ' + ' ' + iBorn + ' ' + 'წლიანი');
+// #7
+if (iBorn < 2000)
+console.log ('მე დავიბადე მე-20 საუკუნეში');
+else (iBorn > 2000)
+console.log ('მე დავიბადე 21-ე საუკუნეში')
+
